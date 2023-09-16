a1ffad5201fc51c480d163b35bc0d92c8ff9d322
Jackt <tktjacky0445@gmail.com>
2023-09-16 14:40:10 -0400

    new update

--- football_etl_dag.py
+++ football_etl_dag.py
@@ -1,13 +1,26 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
+from airflow.opreators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
+import FootballMatchExtractor
+import os
+
+def get_date():
+    day = datetime.today() - timedelta(days=1)
+    return day.strftime("%Y-%m-%d")
 
 def get_football_match_data():
-    import FootballMatchExtractor
     extractor = FootballMatchExtractor.Extractor(link="https://www.theguardian.com/football/results", days_ago=1)
     return extractor.get_data()
+
+def branching_data():
+    file_name = f"match-data-{get_date()}.json"
+    if os.path.exists(file_name):
+        return "upload-file-to-s3"
+    else:
+        return "do-nothing"
 
 default_args = {
     "owner":"admin",
@@ -29,18 +42,31 @@ dag = DAG(
 get_match_data = PythonOperator(
     task_id="extract-football-data",
     python_callable=get_football_match_data,
-    dag=dag,
-    do_xcom_push=True
+    dag=dag
 )
 
-# branching = BranchPythonOperator(
-#     task_id="branching",
-#     python_callable=lambda x: 
-# )
+branching = BranchPythonOperator(
+    task_id="branching",
+    python_callable=branching_data,
+    dag=dag
+)
 
-# upload_match_data = BashOperator(
-#     task_id="upload-football-data",
-#     bash_command=""
-# )
+upload_file = BashOperator(
+    task_id="upload-file-to-s3",
+    bash_command=f"aws s3 ./match-data-{get_date()}.json s3:/// ",
+    dag=dag
+)
 
-get_match_data
+remove_local_file = BashOperator(
+    task="remove_local_file",
+    bash_command=f"rm ./match-data-{get_date()}.json ",
+    dag=dag
+)
+
+do_nothing = DummyOperator(
+    task_id="do-nothing",
+    dag=dag
+)
+
+get_match_data >> branching >> [upload_file, do_nothing]
+upload_file >> remove_local_file
